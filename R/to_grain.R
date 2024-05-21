3bb6137ccdefa176d697fdbf7b9a8f3c9d833258
Victor An <victor.an@sydney.edu.au>
2024-05-21 10:21:32 +1000

    added tograin function

---
+++ R/to_grain.R
@@ -0,0 +1,58 @@
+#' Convert an adjacency matrix or igraph object to a gRain object.
+#' 
+#' @param x An adjacency matrix or an igraph object
+#' @param ... extra parameters to gRain compile
+#' @returns gRain_obj A gRain object.
+#' 
+#' @examples
+#' dag <- bnlearn::model2network("[A][C][F][B|A][D|A:C][E|B:F]")
+#' gRain_obj <- togRain(x = dag |> dagmc::toMatrix(), data = bnlearn::learning.test)
+#' 
+#' @export
+togRain <- function(x, ...) UseMethod("togRain")
+
+#' @export
+togRain.default <- function(x, 
+                            control = list(), 
+                            smooth = 0, 
+                            compile =  TRUE,
+                            details = 0,
+                            data = NULL,
+                            ...
+){
+  togRain.igraph(x, 
+                 control = list(), 
+                 smooth = 0, 
+                 compile =  TRUE,
+                 details = 0,
+                 data = NULL,
+                 ...
+  )
+}
+
+#' @export
+togRain.igraph <- function(x, 
+                           control = list(), 
+                           smooth = 0, 
+                           compile =  TRUE,
+                           details = 0,
+                           data = NULL,
+                           ...
+) {
+  gRbase::compile(gRain::grain(x, control, smooth, compile, details, data, ...))
+  
+}
+
+#' @export  
+togRain.matrix <- function(x, 
+                           control = list(),
+                           smooth = 0,
+                           compile = TRUE,
+                           details = 0,
+                           data = NULL,
+                           ...
+) {
+  igraph_obj <- igraph::graph_from_adjacency_matrix(x)
+  togRain(igraph_obj, control, smooth, compile, details, data, ...)
+}
+
